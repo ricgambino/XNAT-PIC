33a858f2e27563b3a482ad1d33f2c051a0737398
RicGam95 <ric.gambino@gmail.com>
2021-12-14 15:14:47 +0100

    Check for images already processed is introduced

--- xnat_upload.py
+++ xnat_upload.py
@@ -6,6 +6,7 @@ Created on Dec 7, 2021
 
 """
 import shutil, os
+from threading import local
 import xnat
 from glob import glob
 from tkinter import messagebox
@@ -19,7 +20,7 @@ from tkinter import filedialog
 from tqdm import tqdm
 
 
-def xnat_uploader(folder_to_upload, project_id, num_cust_vars, address, user, psw):
+def xnat_uploader(folder_to_upload, project_id, num_cust_vars, address, user, psw, master):
 
     subject_id = '_'.join(os.path.basename(folder_to_upload).split('_')[:-1])
 
@@ -31,6 +32,23 @@ def xnat_uploader(folder_to_upload, project_id, num_cust_vars, address, user, ps
     
     # Check for directories into folder_to_upload path
     list_dirs = os.listdir(folder_to_upload)
+
+    # # Check for resources folder
+    # flag = 0
+    # if 'resources' in list_dirs and os.path.isdir(os.path.join(folder_to_upload, 'resources').replace('\\', '/')):
+    #     results_path = os.listdir(os.path.join(folder_to_upload, 'resources').replace('\\', '/'))
+    #     if results_path!= []:
+    #         results_ans = messagebox.askyesno(
+    #         "Results",
+    #         "Results folder found. Would you like to upload processed images to XNAT?"
+    #     )
+    #     if results_ans == True:
+    #         flag = 1
+    #     else:
+    #         flag = 0
+
+    master._inprogress("Upload in progress")
+
     # Loop over directories
     for dir in tqdm(range(len(list_dirs))):
         # Manage custom variables
@@ -46,67 +64,75 @@ def xnat_uploader(folder_to_upload, project_id, num_cust_vars, address, user, ps
 
         # Get the complete local path of the folder
         local_path = os.path.join(folder_to_upload, list_dirs[dir]).replace('\\', '/')
-        # Check for files into local path
-        file_list = sorted(glob(local_path + '/*', recursive=True))
-        for file in file_list:
-            # progress_bar(file, len(file_list))
-            # file_path = os.path.join(local_path, file).replace('\\', '/')
-            file_path = file.replace('\\', '/')
-            # Check for Date and Time of the study
-            ds = pydicom.dcmread(file_path)
-            if ds.StudyTime != '':
-                # Case 1: Date and Time are explicited                         
-                ds.StudyTime = ds.StudyTime.split('.',1)[0]
-                ds.StudyTime = "_".join(ds.StudyTime.split('.'))
-                ds.StudyDate = "_".join(ds.StudyDate.split('.'))
-                if ds.StudyDate != "" and ds.StudyTime != "":
-                    experiment_id = (
-                        subject_id + "_" + ds.StudyDate + "_" + ds.StudyTime)
-            else:
-                # Case 2: Date and Time are not explicited, so those fields are filled with the current date and time
-                experiment_id = (
-                    subject_id
-                    + "_"
-                    + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-                )
-            # Add custom variables to experiment_id
-            for var in custom_values:
-                experiment_id += "_" + var
+        if 'resources' in local_path:
+            subpaths = os.listdir(local_path)
+            local_path = os.path.join(local_path, subpaths[0]).replace('\\', '/')
+        else:
+            # Check for files into local path
+            file_list = sorted(glob(local_path + '/*', recursive=True))
+            for file in file_list:
+                if file.endswith('.dcm'):
+                    file_path = file.replace('\\', '/')
+
+                    # Check for Date and Time of the study
+                    ds = pydicom.dcmread(file_path)
+                    if ds.StudyTime != '':
+                        # Case 1: Date and Time are explicited                         
+                        ds.StudyTime = ds.StudyTime.split('.',1)[0]
+                        ds.StudyTime = "_".join(ds.StudyTime.split('.'))
+                        ds.StudyDate = "_".join(ds.StudyDate.split('.'))
+                        if ds.StudyDate != "" and ds.StudyTime != "":
+                            experiment_id = (
+                                subject_id + "_" + ds.StudyDate + "_" + ds.StudyTime)
+                    else:
+                        # Case 2: Date and Time are not explicited, so those fields are filled with the current date and time
+                        experiment_id = (
+                            subject_id
+                            + "_"
+                            + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                        )
+                    # Add custom variables to experiment_id
+                    for var in custom_values:
+                        experiment_id += "_" + var
             
-        try:
-            ####################################################################################################
-            # XNAT connection
-            with xnat.connect(server=address, user=user, password=psw) as session:
-                zip_dst = shutil.make_archive(subject_id, "zip", local_path) # .zip file of the current subfolder
+            try:
+                ####################################################################################################
+                # XNAT connection
+                with xnat.connect(server=address, user=user, password=psw) as session:
 
-                project = session.classes.ProjectData(name=project_id, parent=session)
-                subject = session.classes.SubjectData(parent=project, label=subject_id)
-                # Import data to XNAT
-                session.services.import_(zip_dst,
-                                        overwrite="delete", # Overwrite parameter is important!
-                                        project=project_id,
-                                        subject=subject_id,
-                                        experiment=experiment_id,
-                                        content_type='application/zip')
-                subject = project.subjects[subject_id]
-                exp = project.subjects[subject_id].experiments[experiment_id]
+                    zip_dst = shutil.make_archive(subject_id, "zip", local_path) # .zip file of the current subfolder
+                    project = session.classes.ProjectData(name=project_id, parent=session)
+                    subject = session.classes.SubjectData(parent=project, label=subject_id)
+                    # Import data to XNAT
+                    session.services.import_(zip_dst,
+                                            overwrite="delete", # Overwrite parameter is important!
+                                            project=project_id,
+                                            subject=subject_id,
+                                            experiment=experiment_id,
+                                            content_type='application/zip')
+                    subject = project.subjects[subject_id]
+                    exp = project.subjects[subject_id].experiments[experiment_id]
 
-                # Fill custom variables fields
-                for i, element in enumerate(custom_vars):
-                    subject.fields[element] = custom_values[i]
-                    exp.fields[element] = custom_values[i]
-                    # print("custom values ",custom_values[i])
-                os.remove(zip_dst) # Remove temporary .zip file
-            # XNAT connection is closed automatically
-            ####################################################################################################
+                    # Fill custom variables fields
+                    for i, element in enumerate(custom_vars):
+                        subject.fields[element] = custom_values[i]
+                        exp.fields[element] = custom_values[i]
+                        # print("custom values ",custom_values[i])
+                    os.remove(zip_dst) # Remove temporary .zip file
+                    
+                # XNAT connection is closed automatically
+                ####################################################################################################
 
-        except Exception as e:
-            messagebox.showerror("XNAT-PIC - Uploader", e)
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            traceback.print_tb(exc_traceback)
-            sys.exit(1)
+            except Exception as e:
+                messagebox.showerror("XNAT-PIC - Uploader", e)
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                traceback.print_tb(exc_traceback)
+                sys.exit(1)
+
 
     ########
     messagebox.showinfo("XNAT-PIC - Uploader", "DICOM images have been successfully imported to XNAT!")
+
+    master.progress.stop()
 
     return
