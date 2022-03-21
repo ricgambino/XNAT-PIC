8c1d58bc256d74ffe08d37db3e722c573be5a8f8
RicGam95 <ric.gambino@gmail.com>
2022-03-21 15:03:14 +0100

    Introduced function to update caption of the progress bar

--- progress_bar.py
+++ progress_bar.py
@@ -1,16 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 2022
+
+@author: Riccardo Gambino
+"""
+
+"""
+Progress bar class.
+"""
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import HORIZONTAL
 # from threading import Thread
-import time
+# import time
 
 class ProgressBar():
-    def __init__(self, txt_title):
+    def __init__(self, bar_title):
 
-        self.title = txt_title
+        self.title = bar_title
         self.popup = tk.Toplevel()
         self.popup.geometry("%dx%d+%d+%d" % (500, 80, 700, 500))
-        self.popup.title(txt_title)
+        self.popup.title(bar_title)
         self.progress_var = tk.DoubleVar()
         self.bar_length = 400
         self.bar_maximum = 100
@@ -18,7 +28,10 @@ class ProgressBar():
         self.style = ttk.Style()
         self.style.theme_use('alt')
         self.style.configure('blue.Horizontal.TProgressbar', troughcolor  = '#4d4d4d', troughrelief = 'flat', background   = '#2f92ff')
-        # self.popup.mainloop()
+
+    def set_caption(self, index, total):
+        self.label = tk.Label(self.popup, text='Converting ... ' + str(index) + ' / ' + str(total), font=("Calibri", 14, "bold")).place(relx=0.5, rely=0.5, anchor = 's')
+        self.popup.update()
         
     def start_determinate_bar(self):
 
@@ -27,7 +40,6 @@ class ProgressBar():
                                             length=self.bar_length, mode='determinate', maximum=self.bar_maximum)
         self.progressbar.pack(expand=False, fill="x", side="top")
         self.progressbar.place(relx=0.5, rely=0.5, anchor = 'n')
-        # self.popup.update()
 
     def start_indeterminate_bar(self):
 
@@ -40,20 +52,17 @@ class ProgressBar():
         self.progressbar.pack(expand=False, fill="x", side="top")
         self.progressbar.place(relx=0.5, rely=0.5, anchor = 'n')
         self.progressbar.start()
-        # self.progressbar.update()
-        # self.progressbar.mainloop()
 
-    def update_bar(self):
-            self.progressbar.step(0.01)
-            self.progressbar.update()
-        
-        
+    def update_bar(self, step=0.001):
+        self.progressbar.step(step)
+        self.progressbar.update()
+             
     def update_progressbar(self, current_step, max_step):
         progress = int(current_step * self.bar_maximum / max_step)
         self.progress_var.set(progress)
         self.popup.update()
     
     def stop_progress_bar(self):
-        # self.progressbar.stop()
+        self.progressbar.stop()
         self.popup.destroy()
         
