13ade2f01b132ccc078afb8097cbcd0e20c44fe7
RicGam95 <ric.gambino@gmail.com>
2022-03-23 14:57:34 +0100

    First version of the project uploader implemented

--- xnat_uploader.py
+++ xnat_uploader.py
@@ -5,10 +5,13 @@ Created on Dec 7, 2021
 @author: Riccardo Gambino
 
 """
+from numpy import empty
 import pandas as pd
 import os
 from tkinter import messagebox
 import xnat, shutil, time
+from multiprocessing import Pool, cpu_count
+from itertools import repeat
 
 def read_table(path_to_read):
 
@@ -25,8 +28,29 @@ class Dicom2XnatUploader():
     def __init__(self, session):
 
         self.session = session
+        self.n_processes = int(cpu_count() - 1)
 
-    def upload(self, folder_to_upload, project_id):
+    def multi_core_upload(self, folder_to_upload, project_id):
+
+        list_dirs = os.listdir(folder_to_upload)
+        list_of_subjects = [(os.path.join(folder_to_upload, sub).replace('\\', '/'), project_id) for sub in list_dirs]
+
+        start_time = time.time()
+
+        # with Pool(processes = self.n_processes) as pool:
+        #     pool.map(self.uploader, list_of_subjects)
+        for sub in list_of_subjects:
+            self.uploader(sub)
+
+        end_time = time.time()
+        print('Elapsed time for conversion: ' + str(end_time - start_time) + ' s')
+
+    def uploader(self, args):
+
+        folder_to_upload = args[0]
+        project_id = args[1]
+
+        print('Uploading ' + str(folder_to_upload.split('/')[-1]) + ' to ' + str(project_id))
 
         # Check if 'MR' folder is already into the folder_to_upload path
         if 'MR' != os.path.basename(folder_to_upload):
@@ -34,11 +58,19 @@ class Dicom2XnatUploader():
         else:
             folder_to_upload = folder_to_upload.replace('\\', '/')
 
-        # Check for existing file!!!
-        subject_data = read_table('/'.join([folder_to_upload, 'Custom_Variables.txt']))
-
-        subject_id = subject_data['Subject']
-        experiment_id = '_'.join([subject_data['Project'], subject_data['Subject'], subject_data['Group'], subject_data['Timepoint']]).replace(' ', '_')
+        # Check for existing custom variables file
+        try:
+            subject_data = read_table('/'.join([folder_to_upload, 'Custom_Variables.txt']))
+            # Define the subject_id and the experiment_id
+            subject_id = subject_data['Subject']
+            experiment_id = '_'.join([subject_data['Project'], subject_data['Subject'], subject_data['Group'], subject_data['Timepoint']]).replace(' ', '_')
+            flag = 1
+        except Exception as error:
+            # messagebox.showinfo("DICOM Uploader", "Custom Variables are not available in this folder " + str(folder_to_upload))
+            # Define the subject_id and the experiment_id   
+            subject_id = folder_to_upload.split('/')[-1]
+            experiment_id = '_'.join([folder_to_upload.split('/')[-3], folder_to_upload.split('/')[-2]]).replace(' ', '_')
+            flag = 0
 
         project = self.session.classes.ProjectData(
                                         name=project_id, parent=self.session)
@@ -46,35 +78,42 @@ class Dicom2XnatUploader():
                                         parent=project, label=subject_id)
 
         if experiment_id in subject.experiments.key_map.keys():
-        # ALERT! That patient already exists!
-            answer = messagebox.askyesno("XNAT-PIC Uploader", "A patient with the same experiment_id already exists. Do you want to upload it anyway?")
+            # ALERT! That patient already exists!
+            answer = messagebox.askyesno("XNAT-PIC - Uploader", "A patient with the same experiment_id already exists. Do you want to upload it anyway?")
             
             if answer is False:
                 return
 
         start_time = time.time()
 
-        zip_dst = shutil.make_archive(folder_to_upload.split('/')[-2], "zip", folder_to_upload) # .zip file of the current subfolder
+        try:
+            zip_dst = shutil.make_archive(folder_to_upload.split('/')[-2], "zip", folder_to_upload) # .zip file of the current subfolder
+            with xnat.connect(server=self.session._original_uri, jsession=self.session._jsession, cli=True, logger=self.session.logger) as connection:
+                connection.services.import_(zip_dst,
+                                        overwrite="delete", # Overwrite parameter is important!
+                                        project=project_id,
+                                        subject=subject_id,
+                                        experiment=experiment_id,
+                                        content_type='application/zip')
 
-        with xnat.connect(server=self.session._original_uri, jsession=self.session._jsession, cli=True, logger=self.session.logger) as connection:
-            connection.services.import_(zip_dst,
-                                    overwrite="delete", # Overwrite parameter is important!
-                                    project=project_id,
-                                    subject=subject_id,
-                                    experiment=experiment_id,
-                                    content_type='application/zip')
+                experiment = project.subjects[subject_id].experiments[experiment_id]
+                
+                if flag == 1:
+                    for var in subject_data.keys():
+                        if subject_data[var] == '':
+                            continue
+                        if var == 'Project' or var == 'Subject':
+                            subject.fields[var] = subject_data[var]
+                            experiment.fields[var] = subject_data[var]
+                        else:
+                            experiment.fields[var] = subject_data[var]
 
-            experiment = project.subjects[subject_id].experiments[experiment_id]
+            os.remove(zip_dst)
+
+        except Exception as e: 
+            messagebox.showerror("XNAT-PIC - Uploader", e)
             
-            for var in subject_data.keys():
-                # Check for empty custom variables!!!
-                if var == 'Project' or var == 'Subject':
-                    subject.fields[var] = subject_data[var]
-                    experiment.fields[var] = subject_data[var]
-                else:
-                    experiment.fields[var] = subject_data[var]
-
-        os.remove(zip_dst)
-
         end_time = time.time()
         print('Elapsed time: ' + str(end_time - start_time) + ' seconds')
+
+    
