75440e7f90e2c909223060128c3932d1328ee974
fgammaraccio <francesco.gammaraccio@unito.it>
2022-05-19 09:46:03 +0200

    Metadata layout

--- layout_style.py
+++ layout_style.py
@@ -27,6 +27,7 @@ LARGE_FONT = ("Calibri", 22, "bold")
 SMALL_FONT = ("Calibri", 16, "bold")
 SMALL_FONT_2 = ("Calibri", 10)
 SMALL_FONT_3 = ("Calibri", 12)
+SMALL_FONT_4 = ("Calibri", 14, "bold")
 KEYWORD_FONT = ("Calibri", 8)
 ATTACHED_FONT = ("Calibri", 8, "underline")
 CURSOR_HAND = "hand2"
@@ -45,6 +46,7 @@ class MyStyle():
         # Configure Buttons
         self.style.configure('TButton', font=SMALL_FONT)
         self.style.configure("Secondary.TButton", font=SMALL_FONT_3)
+        self.style.configure("Secondary1.TButton", font=SMALL_FONT_4)
         self.style.configure("WithoutBack.TButton", padding=2, background=WHITE, foreground="black", borderwidth=0)
         self.style.map("WithoutBack.TButton", background=[('active', WHITE), ('disabled', WHITE)])
         self.style.configure("Popup.TButton", padding=2, background=WHITE, foreground="black", borderwidth=0, font=SMALL_FONT)
@@ -52,6 +54,7 @@ class MyStyle():
         self.style.configure("MainPopup.TButton", padding=2, font=SMALL_FONT, width=15)
         self.style.configure("Keyword.TButton", font=KEYWORD_FONT, background=WHITE, foreground=theme_colors[style]["colors"]["primary"], borderwidth=1)
         self.style.map("Keyword.TButton", background=[("active", WHITE)], foreground=[("active", theme_colors[style]["colors"]["primary"])])
+        
 
         # Configure Labels
         self.style.configure("Title.TLabel", background=WHITE, foreground="black", font=TITLE_FONT)
