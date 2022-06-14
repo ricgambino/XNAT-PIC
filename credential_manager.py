c978cfb5a98a109651d06595b7c45e83f73ad3ce
RicGam95 <ric.gambino@gmail.com>
2022-06-14 09:58:31 +0200

    Changed input arguments of the CredentialManager class

--- credential_manager.py
+++ credential_manager.py
@@ -8,123 +8,130 @@ Created on May 30, 2022
 import os
 import getpass
 import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap as ttk
+from access_manager import CURSOR_HAND
 from accessory_functions import disable_buttons
 
 
 class CredentialManager():
 
-    def __init__(self):
+    def __init__(self, root):
 
-        popup = tk.Tk()
-        popup.title("XNAT-PIC ~ Credentials")
-        popup.geometry("%dx%d+%d+%d" % (600, 300, 500, 250))
+        self.root = root
 
-        popup.tk_label = tk.Label(popup, text="XNAT-PIC Configuration", font=("Calibri", 20, "bold"))
-        popup.tk_label.grid(row=0, column=1, padx=10, pady=10)
+        self.root.title("XNAT-PIC ~ Credentials")
+        self.root.geometry("+%d+%d" % (500, 250))
+
+        # self.popup = ttk.Toplevel(self.root)
+        self.popup = ttk.Frame(self.root)
+        self.popup.pack(fill='both', expand=1)
+        
+        self.popup.tk_label = ttk.Label(self.popup, text="XNAT-PIC Configuration")
+        self.popup.tk_label.grid(row=0, column=1, padx=10, pady=10)
 
         # XNAT-PIC secretUser
-        popup.label_secretUser = tk.Label(popup, text="User", font=("Calibri", 10))
-        popup.label_secretUser.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
-        popup.entry_secretUser = ttk.Entry(popup, show="", width=50, state='disabled')
-        popup.entry_secretUser.var = tk.StringVar()
-        popup.entry_secretUser["textvariable"] = popup.entry_secretUser.var
-        popup.entry_secretUser.grid(row=1, column=1, padx=5, pady=5)
-        popup.entry_secretUser.var.set(getpass.getuser())
+        self.popup.label_secretUser = ttk.Label(self.popup, text="User")
+        self.popup.label_secretUser.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
+        self.popup.entry_secretUser = ttk.Entry(self.popup, show="", width=50, state='disabled')
+        self.popup.entry_secretUser.var = tk.StringVar()
+        self.popup.entry_secretUser["textvariable"] = self.popup.entry_secretUser.var
+        self.popup.entry_secretUser.grid(row=1, column=1, padx=5, pady=5)
+        self.popup.entry_secretUser.var.set(getpass.getuser())
 
         # User CheckButton
         def enable_user_entry(*args):
-            popup.entry_secretUser.configure(state='normal')
-        popup.change_user_btn_var = tk.IntVar()
-        popup.change_user_btn= tk.Checkbutton(popup, variable=popup.change_user_btn_var, onvalue=1, offvalue=0, text="Change User")
-        popup.change_user_btn.grid(row=1, column=2, padx=5, pady=5, sticky=tk.W)
-        popup.change_user_btn_var.trace('w', enable_user_entry)
+            if self.popup.change_user_btn_var.get() == 1:
+                self.popup.entry_secretUser.configure(state='normal')
+            else:
+                self.popup.entry_secretUser.configure(state='disabled')
+        self.popup.change_user_btn_var = tk.IntVar()
+        self.popup.change_user_btn= ttk.Checkbutton(self.popup, variable=self.popup.change_user_btn_var, onvalue=1, offvalue=0, text="Change User")
+        self.popup.change_user_btn.grid(row=1, column=2, padx=5, pady=5, sticky=tk.W)
+        self.popup.change_user_btn_var.trace('w', enable_user_entry)
         
         # XNAT-PIC secretKey
         def enable_secretPIN(*args):
-            if popup.entry_secretKey_2.var.get() != '':
-                popup.entry_secretPIN.configure(state='normal')
-                popup.entry_secretPIN_2.configure(state='normal')
+            if self.popup.entry_secretKey_2.var.get() != '':
+                self.popup.entry_secretPIN.configure(state='normal')
+                self.popup.entry_secretPIN_2.configure(state='normal')
             else:
-                popup.entry_secretPIN.configure(state='disabled')
-                popup.entry_secretPIN_2.configure(state='disabled')
-        popup.label_secretKey = tk.Label(popup, text="Insert Key", font=("Calibri", 10))
-        popup.label_secretKey.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
-        popup.entry_secretKey = ttk.Entry(popup, show="*", width=50)
-        popup.entry_secretKey.var = tk.StringVar()
-        popup.entry_secretKey["textvariable"] = popup.entry_secretKey.var
-        popup.entry_secretKey.grid(row=2, column=1, padx=5, pady=5)
+                self.popup.entry_secretPIN.configure(state='disabled')
+                self.popup.entry_secretPIN_2.configure(state='disabled')
+        self.popup.label_secretKey = ttk.Label(self.popup, text="Insert Key")
+        self.popup.label_secretKey.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
+        self.popup.entry_secretKey = ttk.Entry(self.popup, show="*", width=50)
+        self.popup.entry_secretKey.var = tk.StringVar()
+        self.popup.entry_secretKey["textvariable"] = self.popup.entry_secretKey.var
+        self.popup.entry_secretKey.grid(row=2, column=1, padx=5, pady=5)
         
         # Re-insert secretKey
         def check_Key(*args):
             try:
-                popup.checked_key.destroy()
+                self.popup.checked_key.destroy()
             except:
                 pass
-            if popup.entry_secretKey_2.var.get() != '':
-                if popup.entry_secretKey.var.get() != popup.entry_secretKey_2.var.get():
-                    popup.checked_key = tk.Label(popup, font=("Calibri", 8, "underline"), text="Password does not correspond!", fg='red')
-                    popup.checked_key.grid(row=3, column=2, padx=5, sticky=tk.W)
+            if self.popup.entry_secretKey_2.var.get() != '':
+                if self.popup.entry_secretKey.var.get() != self.popup.entry_secretKey_2.var.get():
+                    self.popup.checked_key = ttk.Label(self.popup, text="Password does not correspond!", foreground='red')
+                    self.popup.checked_key.grid(row=3, column=2, padx=5, sticky=tk.W)
                 else:
-                    popup.checked_key = tk.Label(popup, font=("Calibri", 8, "underline"), text="OK!", fg='green')
-                    popup.checked_key.grid(row=3, column=2, padx=5, sticky=tk.W)
+                    self.popup.checked_key = ttk.Label(self.popup, text="OK!", foreground='green')
+                    self.popup.checked_key.grid(row=3, column=2, padx=5, sticky=tk.W)
 
-        popup.label_secretKey_2 = tk.Label(popup, text="Insert Key again", font=("Calibri", 10))
-        popup.label_secretKey_2.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W)
-        popup.entry_secretKey_2 = ttk.Entry(popup, show="*", width=50)
-        popup.entry_secretKey_2.var = tk.StringVar()
-        popup.entry_secretKey_2["textvariable"] = popup.entry_secretKey_2.var
-        popup.entry_secretKey_2.grid(row=3, column=1, padx=5, pady=5)
-        popup.entry_secretKey_2.var.trace('w', check_Key)
-        popup.entry_secretKey_2.var.trace('w', enable_secretPIN)
+        self.popup.label_secretKey_2 = ttk.Label(self.popup, text="Insert Key again")
+        self.popup.label_secretKey_2.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W)
+        self.popup.entry_secretKey_2 = ttk.Entry(self.popup, show="*", width=50)
+        self.popup.entry_secretKey_2.var = tk.StringVar()
+        self.popup.entry_secretKey_2["textvariable"] = self.popup.entry_secretKey_2.var
+        self.popup.entry_secretKey_2.grid(row=3, column=1, padx=5, pady=5)
+        self.popup.entry_secretKey_2.var.trace('w', check_Key)
+        self.popup.entry_secretKey_2.var.trace('w', enable_secretPIN)
 
         # XNAT-PIC secretPIN
         def enable_next_btn(*args):
-            if popup.entry_secretPIN_2.var.get() != '':
-                popup.next_btn.configure(state='normal')
+            if self.popup.entry_secretPIN_2.var.get() != '':
+                self.popup.next_btn.configure(state='normal')
             else:
-                popup.next_btn.configure(state='disabled')
-        popup.label_secretPIN = tk.Label(popup, text="Insert PIN", font=("Calibri", 10))
-        popup.label_secretPIN.grid(row=4, column=0, padx=10, pady=5, sticky=tk.W)
-        popup.entry_secretPIN = ttk.Entry(popup, show="*", width=50, state='disabled')
-        popup.entry_secretPIN.var = tk.StringVar()
-        popup.entry_secretPIN["textvariable"] = popup.entry_secretPIN.var
-        popup.entry_secretPIN.grid(row=4, column=1, padx=5)
+                self.popup.next_btn.configure(state='disabled')
+        self.popup.label_secretPIN = ttk.Label(self.popup, text="Insert PIN")
+        self.popup.label_secretPIN.grid(row=4, column=0, padx=10, pady=5, sticky=tk.W)
+        self.popup.entry_secretPIN = ttk.Entry(self.popup, show="*", width=50, state='disabled')
+        self.popup.entry_secretPIN.var = tk.StringVar()
+        self.popup.entry_secretPIN["textvariable"] = self.popup.entry_secretPIN.var
+        self.popup.entry_secretPIN.grid(row=4, column=1, padx=5)
 
         # Re-insert secretPIN
         def check_PIN(*args):
             try:
-                popup.checked_PIN.destroy()
+                self.popup.checked_PIN.destroy()
             except:
                 pass
-            if popup.entry_secretPIN_2.var.get() != '':
-                if popup.entry_secretPIN.var.get() != popup.entry_secretPIN_2.var.get():
-                    popup.checked_PIN = tk.Label(popup, font=("Calibri", 8, "underline"), text="Password does not correspond!", fg='red')
-                    popup.checked_PIN.grid(row=5, column=2, padx=5, sticky=tk.W)
+            if self.popup.entry_secretPIN_2.var.get() != '':
+                if self.popup.entry_secretPIN.var.get() != self.popup.entry_secretPIN_2.var.get():
+                    self.popup.checked_PIN = ttk.Label(self.popup, text="Password does not correspond!", foreground='red')
+                    self.popup.checked_PIN.grid(row=5, column=2, padx=5, sticky=tk.W)
                 else:
-                    popup.checked_PIN = tk.Label(popup, font=("Calibri", 8, "underline"), text="OK!", fg='green')
-                    popup.checked_PIN.grid(row=5, column=2, padx=5, sticky=tk.W)
+                    self.popup.checked_PIN = ttk.Label(self.popup, text="OK!", foreground='green')
+                    self.popup.checked_PIN.grid(row=5, column=2, padx=5, sticky=tk.W)
 
-        popup.label_secretPIN_2 = tk.Label(popup, text="Insert PIN again", font=("Calibri", 10))
-        popup.label_secretPIN_2.grid(row=5, column=0, padx=10, pady=5, sticky=tk.W)
-        popup.entry_secretPIN_2 = ttk.Entry(popup, show="*", width=50, state='disabled')
-        popup.entry_secretPIN_2.var = tk.StringVar()
-        popup.entry_secretPIN_2["textvariable"] = popup.entry_secretPIN_2.var
-        popup.entry_secretPIN_2.grid(row=5, column=1, padx=5)
-        popup.entry_secretPIN_2.var.trace('w', check_PIN)
-        popup.entry_secretPIN_2.var.trace('w', enable_next_btn)
+        self.popup.label_secretPIN_2 = ttk.Label(self.popup, text="Insert PIN again")
+        self.popup.label_secretPIN_2.grid(row=5, column=0, padx=10, pady=5, sticky=tk.W)
+        self.popup.entry_secretPIN_2 = ttk.Entry(self.popup, show="*", width=50, state='disabled')
+        self.popup.entry_secretPIN_2.var = tk.StringVar()
+        self.popup.entry_secretPIN_2["textvariable"] = self.popup.entry_secretPIN_2.var
+        self.popup.entry_secretPIN_2.grid(row=5, column=1, padx=5)
+        self.popup.entry_secretPIN_2.var.trace('w', check_PIN)
+        self.popup.entry_secretPIN_2.var.trace('w', enable_next_btn)
 
         # NEXT Button
         def next():
-            popup.destroy()
-            self.write_credentials(popup.entry_secretUser.var, popup.entry_secretKey.var, popup.entry_secretPIN.var)
-        popup.next_text = tk.StringVar() 
-        popup.next_btn = tk.Button(popup, textvariable=popup.next_text, font=("Calibri", 16), bg="#80FFE6", fg="black", 
-                                    borderwidth=3, command=next, cursor="hand2", takefocus=0, state='disabled')
-        popup.next_text.set("Next")
-        popup.next_btn.grid(row=6, column=1, padx=10, pady=10)
+            self.popup.destroy()
+            self.write_credentials(self.popup.entry_secretUser.var, self.popup.entry_secretKey.var, self.popup.entry_secretPIN.var)
+        self.popup.next_text = tk.StringVar() 
+        self.popup.next_btn = ttk.Button(self.popup, textvariable=self.popup.next_text, command=next, cursor=CURSOR_HAND, takefocus=0, state='disabled')
+        self.popup.next_text.set("Next")
+        self.popup.next_btn.grid(row=6, column=1, padx=10, pady=10)
 
-        popup.mainloop()
     
     def write_credentials(self, user, psw, pin):
 
@@ -137,6 +144,8 @@ class CredentialManager():
             credential_file.write('secretPIN=' + '"' + str(pin.get()) + '"' + '\n')
             credential_file.write('bufferSize1=64\n')
             credential_file.write('bufferSize2=1024')
+
+       
 
     def change_credentials(self, user, psw, pin):
 
